de4378c431b4b793a335a12a867f44b5f7abdc5f
moldovanus <moldovanus@96a9c130-295f-11df-b102-9b0ba5a782e2>
2010-06-23 14:27:41 +0000

    Failed commit: Default

--- src/contextaware/agents/behaviours/ReceiveMessageRLBehaviour.java
+++ src/contextaware/agents/behaviours/ReceiveMessageRLBehaviour.java
@@ -11,15 +11,17 @@ import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import contextaware.GlobalVars;
 import contextaware.agents.ReinforcementLearningAgent;
+import contextaware.worldInterface.datacenterInterface.proxies.ServerManagementProxyInterface;
+import contextaware.worldInterface.datacenterInterface.proxies.impl.ProxyFactory;
 import contextaware.worldInterface.dtos.TaskDto;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.swrl.model.SWRLFactory;
-import ontologyRepresentations.greenContextOntology.*;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import ontologyRepresentations.greenContextOntology.*;
 
 /**
  * @author Moldovanus
@@ -32,11 +34,13 @@ public class ReceiveMessageRLBehaviour extends CyclicBehaviour {
     private SWRLFactory swrlFactory;
     private DatacenterProtegeFactory protegeFactory;
     private OntModel ontModelDataCenter;
+    private OWLModel owlModelDataCenter;
 
     public ReceiveMessageRLBehaviour(Agent agent, OWLModel owlModel, OntModel jenaModel, OWLModel owlModelDataCenter,
                                      OntModel ontModelDataCenter) {
         super(agent);
-        this.protegeFactory = new DatacenterProtegeFactory(owlModelDataCenter);
+        protegeFactory = new DatacenterProtegeFactory(owlModelDataCenter);
+        this.owlModelDataCenter = owlModelDataCenter;
         this.agent = (ReinforcementLearningAgent) agent;
         this.owlModel = owlModel;
         this.jenaModel = jenaModel;
@@ -80,11 +84,11 @@ public class ReceiveMessageRLBehaviour extends CyclicBehaviour {
                         TaskDto taskDto = (TaskDto) message.getContentObject();
                         Task task = protegeFactory.createTask("http://www.owl-ontologies.com/Datacenter.owl#" + taskDto.getTaskName());
                         System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa          " + taskDto.getTaskName());
-                        QoSPolicy policy = protegeFactory.createQoSPolicy(taskDto.getTaskName() + "Policy");
+                        QoSPolicy policy = protegeFactory.createQoSPolicy(taskDto.getTaskName() + "_QoSPolicy");
 
-                        RequestedTaskInfo requestedInfo = protegeFactory.createRequestedTaskInfo(taskDto.getTaskName() + "RequestedInfo");
-                        ReceivedTaskInfo receivedInfo = protegeFactory.createReceivedTaskInfo(taskDto.getTaskName() + "ReceivedInfo");
-
+                        RequestedTaskInfo requestedInfo = protegeFactory.createRequestedTaskInfo(taskDto.getTaskName() + "_RequestedInfo");
+                        ReceivedTaskInfo receivedInfo = protegeFactory.createReceivedTaskInfo(taskDto.getTaskName() + "_ReceivedInfo");
+                        task.setTaskName(taskDto.getTaskName());
 
                         requestedInfo.setCores(taskDto.getRequestedCores(), ontModelDataCenter);
                         requestedInfo.setCpuMaxAcceptableValue(taskDto.getRequestedCPUMax(), ontModelDataCenter);
@@ -115,12 +119,22 @@ public class ReceiveMessageRLBehaviour extends CyclicBehaviour {
                     break;
                 case ACLMessage.INFORM_IF:
                     System.out.println("http://www.owl-ontologies.com/Datacenter.owl#" + message.getContent().split("-")[0]);
+
                     Task selectedTask = protegeFactory.getTask("http://www.owl-ontologies.com/Datacenter.owl#" + message.getContent().split("-")[0]);
                     if (selectedTask.getAssociatedServer() != null) {
                         RemoveTaskFromServerCommand command = new RemoveTaskFromServerCommand(protegeFactory, selectedTask.getName(), selectedTask.getAssociatedServer().getName());
                         command.execute(jenaModel);
                         command.executeOnX3D(agent);
-                        selectedTask.deleteInstance(jenaModel, swrlFactory);
+                        command.executeOnWebService();
+                        ServerManagementProxyInterface management;
+
+                        if (selectedTask.getAssociatedServer() != null) {
+                            management = ProxyFactory.createServerManagementProxy(selectedTask.getAssociatedServer().getServerIPAddress());
+                            management.deleteVirtualMachine(selectedTask.getTaskName());
+                        }
+
+                        selectedTask.delete();
+
 
                         agent.sendAllTasksToClient();
                     } else {
